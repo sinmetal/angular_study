2e01c5684a75270610829ed2062d188365b23c6b
sinmetal <metal.tie@gmail.com>
2018-02-24 16:22:07 +0900

    PugEventのページのテンプレートを追加した refs #1

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { LoadingInterceptor } from './loading-interceptor';
 import { LoadingService } from './services/loading.service';
 import { AreaService } from './services/area.service';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { PugeventComponent } from './pugevent/pugevent.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { ChartsModule } from 'ng2-charts/ng2-charts';
     HomeComponent,
     AreaEditComponent,
     ForecastComponent,
-    UnixTimeDatePipe
+    UnixTimeDatePipe,
+    PugeventComponent
   ],
   imports: [
     ChartsModule,
---
+++ frontend/src/app/pugevent/pugevent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pugevent',
+  templateUrl: './pugevent.component.html',
+  styleUrls: ['./pugevent.component.scss']
+})
+export class PugeventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
